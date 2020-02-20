c02d883548cfecef7cc0847ecf2c84ee5111583d
Maral Sabbagh <contact@maralsabbagh.fr>
2020-02-20 10:54:31 +0100

    Ajout du texte définitif sur la homeSection

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './scss/main.scss';
 import HomeSection from './components/home-section/home-section'
 import Cursor from './components/cursor/cursor'
+import LinkedInIcon from './linkedin-icon.svg'
 
 function App() {
   return (
     <div className="showcase__container">
       <HomeSection />
       <Cursor />
+      <a className="social-icon linkedin" href="https://www.linkedin.com/in/maralsabbagh/" title="Restons connectés"><img  src={LinkedInIcon} alt="LinkedIn" /></a>
+
 
     </div>
   );
--- src/components/home-section/home-section.js
+++ src/components/home-section/home-section.js
@@ -4,26 +4,81 @@ import {
 
 } from "gsap";
 import React, {Component} from 'react'
-
+import Creativity from './creativity.jpg';
 
 class HomeSection extends Component {
     constructor(props){
         super(props);
         this.tl = null;
-        this.titleLetter = null;
+        this.titleLine1 = null;
+        this.titleLine2 = null;
+        this.titleLine3 = null;
+
+        this.figureImg = null;
+        this.contentTitle = null;
+        this.contentSummary = null;
+        this.contentLink = null;
     }
     componentDidMount() {
         this.tl = new TimelineMax();
-        this.tl.fromTo(this.titleLetter, {
-            transform: 'matrix(1,0,0,1,-50,0)',
-            opacity: 0
-        }, {
+        this.tl
+            .fromTo(this.figureImg, {
+                opacity:0,
 
-            duration: .75,
-            transform: 'matrix(1,0,0,1,0,0)',
-            opacity: 1
-        })
-
+                visibility: 'hidden'
+            }, {
+                duration:.5,
+                opacity:1,
+                visibility:'visible'
+            })
+            .fromTo(this.titleLine1, {
+                transform: 'matrix(1,0,0,1,-50,0)',
+                opacity: 0
+            }, {
+                duration: .3,
+                transform: 'matrix(1,0,0,1,0,0)',
+                opacity: 1
+            })// Show titleLine1
+            .fromTo(this.titleLine2, {
+                transform: 'matrix(1,0,0,1,-50,0)',
+                opacity: 0
+            }, {
+                duration: .3,
+                transform: 'matrix(1,0,0,1,0,0)',
+                opacity: 1
+            }) //Show titleLine2
+            .fromTo(this.titleLine3, {
+                transform: 'matrix(1,0,0,1,-50,0)',
+                opacity: 0
+            }, {
+                duration: .3,
+                transform: 'matrix(1,0,0,1,0,0)',
+                opacity: 1
+            }) // Show titleLine3
+            .fromTo(this.contentTitle, {
+                transform: 'matrix(1,0,0,1,-50,0)',
+                opacity: 0
+            }, {
+                duration: .3,
+                transform: 'matrix(1,0,0,1,0,0)',
+                opacity: 1
+            }) // Show contentTitle
+            .fromTo(this.contentSummary, {
+                transform: 'matrix(1,0,0,1,-50,0)',
+                opacity: 0
+            }, {
+                duration: .3,
+                transform: 'matrix(1,0,0,1,0,0)',
+                opacity: 1
+            }) // Show contentSummary
+             .fromTo(this.contentLink, {
+                 transform: 'matrix(1,0,0,1,-50,0)',
+                 opacity: 0
+             }, {
+                 duration: .3,
+                 transform: 'matrix(1,0,0,1,0,0)',
+                 opacity: 1
+             }) // Show contentLink
 
     }
     render(){
@@ -32,41 +87,42 @@ class HomeSection extends Component {
 
                 <section className = "section">
                     <div className="section__figure">
-                        <div className="section__figure__inner" ref={div => this.figTitleContainer = div}>
-                            <h1 className="section__figure__title" ref={h1 => this.titleLetter = h1}>
-                                <span>c</span>
-                                <span>r</span>
-                                <span>e</span>
-                                <span>a</span>
-                                <span>t</span>
-                                <span>i</span>
-                                <span>v</span>
-                                <span>i</span>
-                                <span>t</span>
-                                <span>y</span>
+                        <div className="section__figure__inner">
+                            <h1 className="section__figure__title">
+                                <div className="section__figure__title__line-1" ref={div => this.titleLine1 = div}>
+                                    <span>d</span>
+                                    <span>e</span>
+                                    <span>s</span>
+
+                                </div>
+                                <div className="section__figure__title__line-2" ref={div => this.titleLine2 = div}>
+                                    <span>i</span>
+                                    <span>g</span>
+                                    <span>n</span>
+                                </div>
+                                <div className="section__figure__title__line-3" ref={div => this.titleLine3 = div}>
+                                    <span>e</span>
+                                    <span>r</span>
+
+                                </div>
+
                             </h1>
                         </div>
-                        <figure className="section__figure__img">
-                            <img src={
-                                `https://images.unsplash.com/photo-1505085702750-96d89f461398?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80`
-                            }
-                            alt = "by Joel Filipe" />
+                        <figure className="section__figure__img" ref={figure => this.figureImg = figure}>
+                            <img src={Creativity}
+                            alt = "Unsplash" />
                         </figure>
 
                     </div>
 
                     <div className="section__content">
                         <div className="section__content__inner">
-                            <h2 className="section__content__title">Is humanity</h2>
-                            <p className="section__content__summary">The most powerful resource and for me a need to be fufilled.</p>
-                            <p><a href="http://maralsabbagh.fr" title="Site web actuel">Lien vers une page externe</a></p>
+                            <h2 className="section__content__title" ref={h2 => this.contentTitle = h2}>D'interface</h2>
+                            <p className="section__content__summary" ref={p => this.contentSummary = p}>Créative et aux multiples ressources, recherche un job dans une <strong>agence</strong> de communication <em>créative</em>.</p>
+                            <p className="section__content__link" ref={p => this.contentLink = p}><a className="intro-link" href="http://maralsabbagh.fr" title="Site web actuel">Portfolio</a></p>
                             </div>
-
-
-
                     </div>
                 </section>
-
 
         );
     }
